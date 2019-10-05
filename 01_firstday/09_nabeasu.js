12b1716e3567120c69c53033786991cb87104752
YutakaYamasaki <uminchuvirtual@gmail.com>
2019-10-05 14:02:34 +0800

    secondday

--- 01_firstday/09_nabeasu.js
+++ 01_firstday/09_nabeasu.js
@@ -1,13 +1,14 @@
 const limit = 40;
 
 for(let i = 1; i <= limit; i++) {
+
 if(i % 5 == 0 && /3/.test(i)) {
-console.log("AhoWan");
+    console.log("AhoWan");
 } else if(i % 3 == 0 || /3/.test(i)){
-console.log("Aho")
+    console.log("Aho")
 } else if(i % 5 == 0){
-console.log("Wan");
+    console.log("Wan");
 }else{
-console.log(i)    
+    console.log(i)    
 }
 }
